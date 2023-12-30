55b056c5f899abb0f1d4a43fc4183687ac2733c3
fdkonovalov <tr.penrose@mail.ru>
2023-12-30 03:34:18 +0300

    Based table style + prettiwer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-
 export const App = () => {
     return (
-        <div className=' text-red-100'>
-            Hello world!
+        <div>
+            <table className="border-collapse border border-slate-500 ...">
+                <thead>
+                    <tr>
+                        <th className="border border-slate-600 ...">State</th>
+                        <th className="border border-slate-600 ...">City</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="border border-slate-700 ...">Indiana</td>
+                        <td className="border border-slate-700 ...">
+                            Indianapolis
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border border-slate-700 ...">Ohio</td>
+                        <td className="border border-slate-700 ...">
+                            Columbus
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="border border-slate-700 ...">
+                            Michigan
+                        </td>
+                        <td className="border border-slate-700 ...">Detroit</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-      )
-} 
-
+    )
+}
 
 export default App
