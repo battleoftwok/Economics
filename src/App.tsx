d15f8844153ede078da53591b58328ba1f12a72c
fdkonovalov <tr.penrose@mail.ru>
2024-01-04 02:42:58 +0300

    font added + table style worked

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { Input } from 'antd'
 import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 interface Parameter {
     name: string
@@ -7,37 +9,39 @@ interface Parameter {
     unit: string
 }
 
-const Default = [
+type TableState = { [index: number]: Parameter }
+
+const Default: TableState = Object.assign([
     {
         name: 'Крейсерская скорость',
-        ownAircraft: 1400,
-        prototypeAircraft: 100,
+        ownAircraft: 600,
+        prototypeAircraft: 540,
         unit: 'м/с',
     },
     {
         name: 'Крейсерская высота',
-        ownAircraft: 100,
-        prototypeAircraft: 100,
+        ownAircraft: 8,
+        prototypeAircraft: 8,
         unit: 'км',
     },
     {
         name: 'Расчетная дальность полета',
-        ownAircraft: 100,
-        prototypeAircraft: 100,
+        ownAircraft: 3000,
+        prototypeAircraft: 2869,
         unit: 'км',
     },
     {
         name: 'Масса пустого самолета',
-        ownAircraft: 100,
-        prototypeAircraft: 100,
+        ownAircraft: 13800,
+        prototypeAircraft: 13800,
         unit: 'кг',
     },
-]
+])
 
 export const App = () => {
-    const [init, setInit] = useState<{ [index: number]: Parameter }>(
-        Object.assign(Default)
-    )
+    const [init, setInit] = useState<TableState>(Default)
+
+    const { register, handleSubmit } = useForm()
 
     const handleOnChange = (props: {
         index: number
@@ -53,23 +57,24 @@ export const App = () => {
         })
     }
 
-    console.log(Object.entries(init))
-
     return (
-        <div className="mx-auto w-min">
-            <table className="border-collapse border border-slate-500">
+        <form
+            onSubmit={() => handleSubmit((data) => console.log(data))}
+            className="mx-auto w-min font-jet"
+        >
+            <table className="border-collapse border border-slate-300">
                 <thead>
                     <tr>
-                        <th className="border border-slate-600 ...">
+                        <th className="border border-slate-300 ...">
                             Параметр
                         </th>
-                        <th className="border border-slate-600 px-5 py-3">
+                        <th className="border border-slate-300 px-5 py-3">
                             Проектируемый самолет
                         </th>
-                        <th className="border border-slate-600 px-5 py-3">
+                        <th className="border border-slate-300 px-5 py-3">
                             Прототип
                         </th>
-                        <th className="border border-slate-600 whitespace-nowrap px-5">
+                        <th className="border border-slate-300 whitespace-nowrap px-5">
                             Ед. измерения
                         </th>
                     </tr>
@@ -77,49 +82,56 @@ export const App = () => {
                 <tbody className="text-center">
                     {Object.entries(init).map((param, i) => (
                         <tr key={String(param[1].name + i)}>
-                            <td className="border border-slate-700 whitespace-nowrap px-5">
+                            <td className="border border-slate-300 whitespace-nowrap px-5">
                                 {param[1].name}
                             </td>
 
-                            <td className="border border-slate-700">
-                                <input
+                            <td className="border border-slate-300">
+                                <Input
                                     defaultValue={param[1].ownAircraft}
-                                    onChange={(event) =>
-                                        handleOnChange({
-                                            index: i,
-                                            paramName: 'ownAircraft',
-                                            newValue: Number(
-                                                event.currentTarget.value
-                                            ),
-                                        })
-                                    }
-                                    className={`text-center`}
+                                    {...register(`Own.${param[1].name}`, {
+                                        onChange: (event) =>
+                                            handleOnChange({
+                                                index: i,
+                                                paramName: 'ownAircraft',
+                                                newValue: Number(
+                                                    event.currentTarget.value
+                                                ),
+                                            }),
+                                    })}
+                                    className={`font-jet text-center border-white hover:border-slate-100 hover:bg-slate-100
+                                                 focus:bg-white rounded-none focus:border-white`}
                                 />
                             </td>
 
-                            <td className="border border-slate-700">
-                                <input 
+                            <td className="border border-slate-300">
+                                <Input
                                     defaultValue={param[1].prototypeAircraft}
-                                    onChange={(event) =>
-                                        handleOnChange({
-                                            index: i,
-                                            paramName: 'prototypeAircraft',
-                                            newValue: Number(
-                                                event.currentTarget.value
-                                            ),
-                                        })
-                                    }
-                                    className=" text-center" />
+                                    {...register(`Prototype.${param[1].name}`, {
+                                        onChange: (event) =>
+                                            handleOnChange({
+                                                index: i,
+                                                paramName: 'ownAircraft',
+                                                newValue: Number(
+                                                    event.currentTarget.value
+                                                ),
+                                            }),
+                                    })}
+                                    className="font-jet text-center border-white hover:border-slate-100 hover:bg-slate-100
+                                                 focus:bg-white rounded-none focus:border-white"
+                                />
                             </td>
 
-                            <td className="border border-slate-700">
+                            <td className="border border-slate-300">
                                 {param[1].unit}
                             </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
-        </div>
+
+            <button type="submit">Нажми</button>
+        </form>
     )
 }
 
